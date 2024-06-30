67c677c493c1f8bdc4e6a1d93a78682851901152
Adrian Ong <ongadrian98@gmail.com>
2024-07-01 03:51:18 +0800

    Adapted dropout

--- experiments/run_clm.py
+++ experiments/run_clm.py
@@ -466,7 +466,7 @@ def main():
             peft_config = LoraConfig(
                 r=8,
                 lora_alpha=16,
-                lora_dropout=0.1,
+                lora_dropout=0.1 if not data_args.dropout_debias else 0.15, # Standardized dropout to match
                 target_modules=target_modules,
                 bias="none",
                 task_type="CAUSAL_LM",
